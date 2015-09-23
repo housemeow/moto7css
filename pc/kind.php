a2f54a636d75a3fd5d67a957b66bd5124f82755d
KellyYang <housemeow@gmail.com>
2015-09-23 22:10:59 +0800

    hot keyword

--- pc/kind.php
+++ pc/kind.php
@@ -11,15 +11,22 @@ $upload_path="../upload/"
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <!--css-->
         <link href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css" rel="stylesheet">
+
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
         <link href="js_fk/bxslider/jquery.bxslider.css" rel="stylesheet">
         <link rel="stylesheet/less" type="text/css" href="css/style.less" />
         <!--js-->
-        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.5/less.min.js"></script>
-        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-        <script src="https://rawgit.com/leafo/sticky-kit/v1.0.4/jquery.sticky-kit.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.5/less.min.js"></script> 
+
+        <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script> 
+
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script> 
+
+        <script src="https://rawgit.com/leafo/sticky-kit/v1.0.4/jquery.sticky-kit.min.js"></script> 
+
         <script src="js_fk/bxslider/jquery.bxslider.min.js"></script>
         <script src="js/function.js"></script>
     </head>
@@ -116,12 +123,14 @@ $upload_path="../upload/"
                                 <input type="text" class="form-control" placeholder="請輸入關鍵字查詢">
                                 <img src="<?php echo $images_path ?>search_icon.png">
                                 
-                                <div>熱門搜尋</div>
-                                <ul>
-                                    <li><span class="active">國產車款</span></li>
-                                    <li><span>女性騎乘</span></li>
-                                    <li><span>重型機車</span></li>
-                                </ul>
+                                <div class="hot-search">
+                                    <div>熱門搜尋：</div>
+                                    <ul>
+                                        <li><span class="active">國產車款</span></li>
+                                        <li><span>女性騎乘</span></li>
+                                        <li><span>重型機車</span></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                         
