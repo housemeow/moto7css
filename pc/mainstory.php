b6207d59e717a423575152d42767786d709460d7
KemingChen <believe75467@gmail.com>
2015-10-06 19:39:02 +0800

    add some mainstory view

---
+++ pc/mainstory.php
@@ -0,0 +1,102 @@
+<?php
+$images_path="images/";
+$upload_path="../upload/"
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Title of the document</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<!--css-->
+		<link href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<link href="js_fk/bxslider/jquery.bxslider.css" rel="stylesheet">
+		<link rel="stylesheet/less" type="text/css" href="css/mainstory.less" />
+		<!--js-->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.5/less.min.js"></script>
+		<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		<script src="https://rawgit.com/leafo/sticky-kit/v1.0.4/jquery.sticky-kit.min.js"></script>
+		<script src="js_fk/bxslider/jquery.bxslider.min.js"></script>
+		<script src="js/function.js"></script>
+	</head>
+	<body>
+		<div class="mainstory">
+			<ul class="tab-panel">
+				<li class="active">全部資訊</li><!-- 
+				 --><li>即時新聞</li><!-- 
+				 --><li>專題報導</li><!-- 
+				 --><li>討論專區</li><!-- 
+				 --><li>好友動態</li>
+			</ul>
+			<ul class="block-panel">
+			    <li class="left-image-panel">
+			        <img class="left-photo" src="<?=$upload_path?>news_photo_1.jpg" />
+			        <div class="right-info background">
+			            <div class="title">
+			                <div class="tag">
+			                    <div>新聞</div><!-- 
+				 				--><div>部品</div>
+							</div>
+			                <div class="info">GRO 全球賽車油與WeBike摩托百貨合作</div>
+			            </div>
+			            <div class="content">
+			                摩托車綜合購物網站「Webike」自2000年在日本開幕，14年來已成為日......<font class="more">[ more ]</font>
+			            </div>
+			        </div>
+			    </li>
+			    <li class="right-image-panel">
+			        <div class="left-info background">
+			        	<div class="title">
+			                <div class="tag">
+			                    <div>專題</div><!-- 
+				 				--><div>部品</div>
+							</div>
+			                <div class="info">MTOT7 教練團日本 Suzuka 安駕訓練受訓紀實</div>
+			            </div>
+			            <div class="content">
+			                為了精進MOTO7 教練團騎乘技術，提升騎術訓練活動以及安全駕駛內容，MOTO7 主編楊斌與安駕講師小天特別安排到日本的Suzuka 安全駕駛教育中心，來學習最新的騎乘技巧與觀念<font class="more">[ more ]</font>
+			            </div>
+			        </div>
+			        <img class="right-photo" src="<?=$upload_path?>news_photo_2.jpg" />
+			    </li>
+			    <li class="gallery-panel">
+			    	<div class="top-info background">
+						<div class="title">
+			                <div class="tag">
+			                    <div>專題</div><!-- 
+				 				--><div>部品</div>
+							</div>
+			                <div class="info">SYM 野狼SB300/CR 暨重車聯盟發表會報導</div>
+			            </div>
+			    	</div>
+			    	<div class="bottom-photos">
+			    		<img src="<?=$upload_path?>news_photo_3.jpg" />
+				        <img src="<?=$upload_path?>news_photo_4.jpg" />
+				        <img src="<?=$upload_path?>news_photo_5.jpg" />
+				        <img src="<?=$upload_path?>news_photo_6.jpg" />
+				        <img src="<?=$upload_path?>news_photo_7.jpg" />
+			    	</div>
+			    </li>
+			    <li class="left-image-panel">
+			        <img class="left-photo" src="<?=$upload_path?>news_photo_1.jpg" />
+			        <div class="right-info background">
+			            <div class="title">
+			                <div class="tag">
+			                    <div>新聞</div><!-- 
+				 				--><div>部品</div>
+							</div>
+			                <div class="info">GRO 全球賽車油與WeBike摩托百貨合作</div>
+			            </div>
+			            <div class="content">
+			                摩托車綜合購物網站「Webike」自2000年在日本開幕，14年來已成為日......<font class="more">[ more ]</font>
+			            </div>
+			        </div>
+			    </li>
+			</ul>
+		</div>
+	</body>
+</html>
