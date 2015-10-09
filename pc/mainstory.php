3a3a232f87ab3835a2fc0f048d665a8480edbb14
KemingChen <believe75467@gmail.com>
2015-10-09 19:59:54 +0800

    finish mainstory discuss cell

--- pc/mainstory.php
+++ pc/mainstory.php
@@ -44,7 +44,7 @@ $upload_path="../upload/"
 			                <div class="info">GRO 全球賽車油與WeBike摩托百貨合作</div>
 			            </div>
 			            <div class="content">
-			                摩托車綜合購物網站「Webike」自2000年在日本開幕，14年來已成為日......<font class="more">[ more ]</font>
+			                摩托車綜合購物網站「Webike」自2000年在日本開幕，14年來已成為日......<span class="more">[ more ]</span>
 			            </div>
 			        </div>
 			    </li>
@@ -55,10 +55,10 @@ $upload_path="../upload/"
 			                    <div>專題</div><!-- 
 				 				--><div>部品</div>
 							</div>
-			                <div class="info">MTOT7 教練團日本 Suzuka 安駕訓練受訓紀實</div>
+			                <div class="info">MOTO7 教練團日本 Suzuka 安駕訓練受訓紀實</div>
 			            </div>
 			            <div class="content">
-			                為了精進MOTO7 教練團騎乘技術，提升騎術訓練活動以及安全駕駛內容，MOTO7 主編楊斌與安駕講師小天特別安排到日本的Suzuka 安全駕駛教育中心，來學習最新的騎乘技巧與觀念<font class="more">[ more ]</font>
+			                為了精進MOTO7 教練團騎乘技術，提升騎術訓練活動以及安全駕駛內容，MOTO7 主編楊斌與安駕講師小天特別安排到日本的Suzuka 安全駕駛教育中心，來學習最新的騎乘技巧與觀念...<span class="more">[ more ]</span>
 			            </div>
 			        </div>
 			        <img class="right-photo" src="<?=$upload_path?>news_photo_2.jpg" />
@@ -81,6 +81,61 @@ $upload_path="../upload/"
 				        <img src="<?=$upload_path?>news_photo_7.jpg" />
 			    	</div>
 			    </li>
+			    <li class="disscuss-panel">
+			    	<div class="center-info background">
+				    	<div class="title">
+			                <div class="tag">
+			                    <div>討論</div><!-- 
+				 				--><div>部品</div>
+							</div>
+			                <div class="info">SYM 野狼SB300/CR 暨重車聯盟發表會報導</div>
+			            </div>
+			            <div class="disscuss-table">
+			            	<div class="disscuss-row">
+			            		<div class="disscuss-cell">
+			            			<img src="<?=$upload_path?>discuss_cell_photo_1.jpg" />
+			            			<div class="content">
+			            				台灣如果125cc能上高速的話 不守規矩的屁孩橫行......
+			            			</div>
+			            		</div>
+			            		<div class="disscuss-cell">
+			            			<img src="<?=$upload_path?>discuss_cell_photo_2.jpg" />
+			            			<div class="content">
+			            				我認為以車速分流來說，這樣可行的但是以台灣的.....
+			            			</div>
+			            		</div>
+			            	</div>
+			            	<div class="disscuss-row">
+			            		<div class="disscuss-cell">
+			            			<img src="<?=$upload_path?>discuss_cell_photo_1.jpg" />
+			            			<div class="content">
+			            				台灣如果125cc能上高速的話 不守規矩的屁孩橫行......
+			            			</div>
+			            		</div>
+			            		<div class="disscuss-cell">
+			            			<img src="<?=$upload_path?>discuss_cell_photo_2.jpg" />
+			            			<div class="content">
+			            				我認為以車速分流來說，這樣可行的但是以台灣的.....
+			            			</div>
+			            		</div>
+			            	</div>
+			            	<div class="disscuss-row">
+			            		<div class="disscuss-cell">
+			            			<img src="<?=$upload_path?>discuss_cell_photo_1.jpg" />
+			            			<div class="content">
+			            				台灣如果125cc能上高速的話 不守規矩的屁孩橫行......
+			            			</div>
+			            		</div>
+			            		<div class="disscuss-cell">
+			            			<img src="<?=$upload_path?>discuss_cell_photo_2.jpg" />
+			            			<div class="content">
+			            				我認為以車速分流來說，這樣可行的但是以台灣的.....
+			            			</div>
+			            		</div>
+			            	</div>
+			            </div>
+		            </div>
+			    </li>
 			    <li class="left-image-panel">
 			        <img class="left-photo" src="<?=$upload_path?>news_photo_1.jpg" />
 			        <div class="right-info background">
@@ -92,7 +147,7 @@ $upload_path="../upload/"
 			                <div class="info">GRO 全球賽車油與WeBike摩托百貨合作</div>
 			            </div>
 			            <div class="content">
-			                摩托車綜合購物網站「Webike」自2000年在日本開幕，14年來已成為日......<font class="more">[ more ]</font>
+			                摩托車綜合購物網站「Webike」自2000年在日本開幕，14年來已成為日......<span class="more">[ more ]</span>
 			            </div>
 			        </div>
 			    </li>
