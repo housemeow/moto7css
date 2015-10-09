5fb915b5ecb3d9647c972272c65a17aa178c0da4
he19911013 <he19911013@gmail.com>
2015-10-09 19:06:36 +0800

    ad-article, 300x250ad, 644x115ad, article_slide_show

---
+++ pc/article_slide_show.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Title of the document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <!--css-->
+    <link href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="js_fk/bxslider/jquery.bxslider.css" rel="stylesheet">
+    <link rel="stylesheet/less" type="text/css" href="css/article_slide_show.less" />
+    <!--js-->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.5/less.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="https://rawgit.com/leafo/sticky-kit/v1.0.4/jquery.sticky-kit.min.js"></script>
+    <script src="js_fk/bxslider/jquery.bxslider.min.js"></script>
+    <script src="js/function.js"></script>
+  </head>
+  <body>
+    <div class="article_slide_show">
+      <div class="top_col">
+        <div class="left_part">
+          <img src="images/japantravel.png" />
+        </div>
+        <div class="read">
+          <a href=""><p>2014 TMAX 530 車主日本GP之旅：茂木賽道篇</p></a>
+          <a href=""><img src="images/readmore.png"></a>
+        </div>
+      </div>
+      <div class="right_part">
+        <a href=""><div class="part">
+          <img src="images/car1.png">
+          <p class="content">CUSTOM MOTO：KYMCO QUANNON BY禾倉技研</p>
+        </div></a>
+        <a href=""><div class="part">
+          <img src="images/car2.png">
+          <p class="content">GRO 全球賽車油與WEBIKE 摩托百貨合作</p>
+        </div></a>
+        <a href=""><div class="part">
+          <img src="images/car3.png">
+          <p class="content">全新BMW R1200 RS – 旅跑車的全新演繹</p>
+        </div></a>
+        <a href=""><div class="part">
+          <img src="images/car4.png">
+          <p class="content">完美新境界：2015 BMW S1000RR</p>
+        </div></a>
+      </div>
+    </div>
+  </body>
+</html>
