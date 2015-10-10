d13b1703d21b48ffe19203d28c62a08184502da1
KellyYang <housemeow@gmail.com>
2015-10-10 17:37:09 +0800

    add hot-article component

---
+++ pc/component-hot-article.php
@@ -0,0 +1,78 @@
+<?php
+$images_path="images/";
+$upload_path="../upload/"
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Hot article</title>
+		<!--css-->
+		<link href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<link href="js_fk/bxslider/jquery.bxslider.css" rel="stylesheet">
+		<link rel="stylesheet/less" type="text/css" href="css/component-hot-article.less" />
+		<!--js-->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.5/less.min.js"></script>
+		<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		<script src="https://rawgit.com/leafo/sticky-kit/v1.0.4/jquery.sticky-kit.min.js"></script>
+		<script src="js_fk/bxslider/jquery.bxslider.min.js"></script>
+		<script src="js/function.js"></script>
+	</head>
+	<body>
+		<div class="hot-article">
+			<div class="hot-article-header">
+				<div class="icon"></div>
+				<span class="title">熱門話題</span>
+				<div class="splitter"></div>
+			</div>
+			<div class="hot-article-content">
+				<ul>
+					<li><a href="#">
+						<div class="icon hot"></div>
+						<div class="title">感受速度與聲浪的極限 台灣TMAX 530 GP應援團</div></a>
+					</li>
+					<li><a href="#">
+						<div class="icon hot"></div>
+						<div class="title">劃時代三腳獸！TRICITY 125 海外試駕分享</div></a>
+					</li>
+					<li><a href="#">
+						<div class="icon hot"></div>
+						<div class="title">DUCATI新技術! 可變氣門正時系統 DVT</div></a>
+					</li>
+					<li><a href="#">
+						<div class="icon"></div>
+						<div class="title">[2014 科隆車展] DUCATI 玩樂至上主義 Scrambler</div></a>
+					</li>
+					<li><a href="#">
+						<div class="icon"></div>
+						<div class="title">SMAX的台北十大美食小吃之旅</div></a>
+					</li>
+					<li><a href="#">
+						<div class="icon"></div>
+						<div class="title">注意，必看！水冷速克達版規 V1.4</div></a>
+					</li>
+					<li><a href="#">
+						<div class="icon"></div>
+						<div class="title">相同的DNA、不同的精采靈魂［跨界跑旅SMAX小劇場］</div></a>
+					</li>
+					<li><a href="#">
+						<div class="icon"></div>
+						<div class="title">SMAX155 DIY系列 傳動拆裝過程分享</div></a>
+					</li>
+					<li><a href="#">
+						<div class="icon"></div>
+						<div class="title">SMAX 155 清完節流閥怠速問題</div></a>
+					</li>
+					<li><a href="#">
+						<div class="icon"></div>
+						<div class="title">smax大燈問題</div></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</body>
+</html>
