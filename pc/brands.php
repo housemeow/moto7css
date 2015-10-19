b8a0e9df12c5812df7dec84f82d5352638accbb1
KellyYang <housemeow@gmail.com>
2015-10-19 17:24:34 +0800

    add brand content component to brands page

--- pc/brands.php
+++ pc/brands.php
@@ -82,6 +82,128 @@ $upload_path="../upload/"
             <div class="container">
                 <div class="two-left">
                     <div class="component-container">
+                        <div class="brands-content">
+                            <div class="brands">
+                                <div class="content-header">
+                                    <a href="#"><img src="<?php echo $images_path ?>taiwan-brands.png"></a>
+                                    <span><b>TAIWAN BRANDS</b></span>
+                                </div>
+                                <div class="content-block">
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>kymco.jpg"></a>
+                                        <span>光陽</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>tw-yamaha.jpg"></a>
+                                        <span>台灣YAMAHA</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>sym.jpg"></a>
+                                        <span>三陽</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>suzuki.jpg"></a>
+                                        <span>鈴木</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>aeon.jpg"></a>
+                                        <span>宏佳騰</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>motive.jpg"></a>
+                                        <span>摩特動力</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>hartford.jpg"></a>
+                                        <span>哈特佛</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="brands">
+                                <div class="content-header">
+                                    <a href="#"><img src="<?php echo $images_path ?>world-brands.png"></a>
+                                    <span><b>WORLD BRANDS</b></span>
+                                </div>
+                                <div class="content-block">
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>yamaha.jpg"></a>
+                                        <span>YAMAHA</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>honda.jpg"></a>
+                                        <span>HONDA</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>ducati.jpg"></a>
+                                        <span>DUCATI</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>kawasaki.jpg"></a>
+                                        <span>KAWASAKI</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>triuph.jpg"></a>
+                                        <span>TRIUMPH</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>ktm.jpg"></a>
+                                        <span>KTM</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>aprilia.jpg"></a>
+                                        <span>APRILIA</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>bmw.jpg"></a>
+                                        <span>BMW</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>mv.jpg"></a>
+                                        <span>MV AGUSTA</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>husqvarna.jpg"></a>
+                                        <span>HUSQVARNA</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>harley.jpg"></a>
+                                        <span>HARLEY-DAVIDSON</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="clothing-brands">
+                                <div class="content-header">
+                                    <a href="#"><img src="<?php echo $images_path ?>clothing-brands.png"></a>
+                                    <span><b>PART&CLOTHING BRANDS</b></span>
+                                </div>
+                                <div class="content-block">
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>motorex.jpg"></a>
+                                        <span>MOTOREX</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>brembo.jpg"></a>
+                                        <span>BREMBO</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>dainese.jpg"></a>
+                                        <span>DAINESE</span>
+                                    </div>
+                                    <div class="block">
+                                        <a href="#"><img src="<?php echo $images_path ?>frando.jpg"></a>
+                                        <span>FRANDO</span>
+                                    </div>
+                                    <ul class="pagination-bar">
+                                        <li><a href="#"></a></li>
+                                        <li><a href="#"></a></li>
+                                        <li><a href="#"></a></li>
+                                        <li><a href="#"></a></li>
+                                        <li><a href="#"></a></li>
+                                        <li><a href="#"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="two-right">
