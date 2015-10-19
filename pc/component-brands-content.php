b7acf2376bcd36066b2205fc203fc63235844dc9
KellyYang <housemeow@gmail.com>
2015-10-19 16:42:42 +0800

    brands content component

---
+++ pc/component-brands-content.php
@@ -0,0 +1,150 @@
+<?php
+$images_path="images/";
+$upload_path="../upload/"
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Title of the document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <!--css-->
+    <link href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="js_fk/bxslider/jquery.bxslider.css" rel="stylesheet">
+    <link rel="stylesheet/less" type="text/css" href="css/component-brands-content.less" />
+    <!--js-->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.5/less.min.js"></script>
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script src="https://rawgit.com/leafo/sticky-kit/v1.0.4/jquery.sticky-kit.min.js"></script>
+    <script src="js_fk/bxslider/jquery.bxslider.min.js"></script>
+    
+  </head>
+  <body>
+    <div class="brands-content">
+      <div class="brands">
+        <div class="content-header">
+          <a href="#"><img src="<?php echo $images_path ?>taiwan-brands.png"></a>
+          <span><b>TAIWAN BRANDS</b></span>
+        </div>
+        <div class="content-block">
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>kymco.jpg"></a>
+            <span>光陽</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>tw-yamaha.jpg"></a>
+            <span>台灣YAMAHA</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>sym.jpg"></a>
+            <span>三陽</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>suzuki.jpg"></a>
+            <span>鈴木</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>aeon.jpg"></a>
+            <span>宏佳騰</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>motive.jpg"></a>
+            <span>摩特動力</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>hartford.jpg"></a>
+            <span>哈特佛</span>
+          </div>
+        </div>
+      </div>
+      <div class="brands">
+        <div class="content-header">
+          <a href="#"><img src="<?php echo $images_path ?>world-brands.png"></a>
+          <span><b>WORLD BRANDS</b></span>
+        </div>
+        <div class="content-block">
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>yamaha.jpg"></a>
+            <span>YAMAHA</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>honda.jpg"></a>
+            <span>HONDA</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>ducati.jpg"></a>
+            <span>DUCATI</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>kawasaki.jpg"></a>
+            <span>KAWASAKI</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>triuph.jpg"></a>
+            <span>TRIUMPH</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>ktm.jpg"></a>
+            <span>KTM</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>aprilia.jpg"></a>
+            <span>APRILIA</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>bmw.jpg"></a>
+            <span>BMW</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>mv.jpg"></a>
+            <span>MV AGUSTA</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>husqvarna.jpg"></a>
+            <span>HUSQVARNA</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>harley.jpg"></a>
+            <span>HARLEY-DAVIDSON</span>
+          </div>
+        </div>
+      </div>
+      <div class="clothing-brands">
+        <div class="content-header">
+          <a href="#"><img src="<?php echo $images_path ?>clothing-brands.png"></a>
+          <span><b>PART&CLOTHING BRANDS</b></span>
+        </div>
+        <div class="content-block">
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>motorex.jpg"></a>
+            <span>MOTOREX</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>brembo.jpg"></a>
+            <span>BREMBO</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>dainese.jpg"></a>
+            <span>DAINESE</span>
+          </div>
+          <div class="block">
+            <a href="#"><img src="<?php echo $images_path ?>frando.jpg"></a>
+            <span>FRANDO</span>
+          </div>
+          <ul class="pagination-bar">
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
