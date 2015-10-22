07f7026e5f2cd8624598e9d2774315f106391b69
he19911013 <he19911013@gmail.com>
2015-10-22 14:37:24 +0800

    format component-ad-article

--- pc/component-ad-article.php
+++ pc/component-ad-article.php
@@ -1,38 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Title of the document</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <!--css-->
-    <link href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css" rel="stylesheet">
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link href="js_fk/bxslider/jquery.bxslider.css" rel="stylesheet">
-    <link rel="stylesheet/less" type="text/css" href="css/component-ad-article.less" />
-    <!--js-->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.5/less.min.js"></script>
-    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-    <script src="https://rawgit.com/leafo/sticky-kit/v1.0.4/jquery.sticky-kit.min.js"></script>
-    <script src="js_fk/bxslider/jquery.bxslider.min.js"></script>
-    <script src="js/function.js"></script>
-  </head>
-  <body>
-    <div class="ad-article">
-      <div class="activity">
-        <div class="redbox"></div>
-        <p>活動特區 <img src="images/dotline.png"></p>
-        <div class="moto">
-          <img src="images/moto7.png">
-        </div>
-        <div class="text"><p>MOTO7 騎乘技術訓練 2013年度開課</p></div>
-        <div class="signup">
-          <p>報名時間: 即日起到10月底為止</p>
-        </div>
-        <div class="signupclick"><a href=""><img src="images/signup.png"> </a></div>
-      </div>
-    </div>
-  </body>
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Title of the document</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+		<!--css-->
+		<link href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<link href="js_fk/bxslider/jquery.bxslider.css" rel="stylesheet">
+		<link rel="stylesheet/less" type="text/css" href="css/component-ad-article.less" />
+		<!--js-->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.5/less.min.js"></script>
+		<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		<script src="https://rawgit.com/leafo/sticky-kit/v1.0.4/jquery.sticky-kit.min.js"></script>
+		<script src="js_fk/bxslider/jquery.bxslider.min.js"></script>
+		<script src="js/function.js"></script>
+	</head>
+	<body>
+		<div class="ad-article">
+			<div class="activity">
+				<div class="redbox"></div>
+				<p>活動特區<img src="images/dotline.png"></p>
+				<div class="moto">
+					<img src="images/moto7.png">
+				</div>
+				<div class="text"><p>MOTO7 騎乘技術訓練 2013年度開課</p></div>
+				<div class="signup">
+					<p>報名時間: 即日起到10月底為止</p>
+				</div>
+				<div class="signupclick"><a href=""><img src="images/signup.png"> </a></div>
+			</div>
+		</div>
+	</body>
 </html>
